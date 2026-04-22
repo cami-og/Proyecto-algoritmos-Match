690011427d8edfcf69aa5366c5d0061aab9b8d12
cami-og <camilaolivares2007@gmail.com>
2026-04-22 08:59:48 -0600

    Add arrays for various attributes in main.cpp
    arreglos vacíos

--- main.cpp
+++ main.cpp
@@ -6,6 +6,17 @@
 
 
 int main{
-  
+  char nombres[][10]={"Camila"," "}
+  int edad[]={18,}
+  int espiritualidad[]={1,2,3}
+  int altura[]={1.55}
+  int escuela[]={1,2,3,4,5}
+  int hobby[]={1,2,3}
+  int personalidad[]={1,2,3}
+  int psexual[]={1,2,3}
+  int genero[]={1,2,3}
+  int pchilaquiles[]={1,2,3}
+  int mascotas[]={1,2,3}
+  int vida[]={1,2,3}
 }
 
