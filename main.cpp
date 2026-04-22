1e800bcdae58b9cbae6a3ed2d64f42593649bda7
cami-og <camilaolivares2007@gmail.com>
2026-04-22 09:11:38 -0600

    Add a new hobby option to the hobby array

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main{
   int espiritualidad[]={1,2,3};
   int altura[]={1.55};
   int escuela[]={1,2,3,4,5};
-  int hobby[]={1,2,3};
+  int hobby[]={1,2,3,4};
   int personalidad[]={1,2,3};
   int psexual[]={1,2,3};
   int genero[]={1,2,3};
