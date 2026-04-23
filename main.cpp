90c173bd2edde956c62ff08a327a1075e266c94b
cami-og <camilaolivares2007@gmail.com>
2026-04-23 17:05:17 -0600

    Update arrays with more names and attributes
    Expanded arrays for names, ages, spirituality, and other attributes.

--- main.cpp
+++ main.cpp
@@ -6,17 +6,17 @@
 
 
 int main{
-  char nombres[][10]={"Camila"," "};
-  int edad[]={18,};
-  int espiritualidad[]={1,2,3};
-  int altura[]={1.55};
-  int escuela[]={1,2,3,4,5};
-  int hobby[]={1,2,3,4};
-  int personalidad[]={1,2,3};
-  int psexual[]={1,2,3};
-  int genero[]={1,2,3};
-  int pchilaquiles[]={1,2,3};
-  int mascotas[]={1,2,3};
-  int vida[]={1,2,3};
+  char nombres[][10]={"Camila","Dani","Fernanda","Ricardo","Valentina","Angel","Carolina","Emiliano","Sara","Ashley","Emiliano","Irwing","Luis","Daniela","Alondra","Luis","Hannah","Aimeé","Hana","Fernanda","Fernanda","Emmanuel","Edwin","Elihu","Amanda","Omar","Elizabeth"," "," "," "};
+  int edad[]={18,20,19,21,21,19,19,21,18,18,21,26,16,18,18,24,24,18,18,18,18,19,26,23,19,20,18, , , };
+  int espiritualidad[]={3,3,1,3,3,1,1,3,1,3,3,1,2,1,1,3,1,3,1,2,2,1,3,3,1,3,3, , , };
+  int altura[]={1.55,1.59,1.6,1.73,1.65,1.8,1.58,1.69,1.6,1.6,1.9};
+  int escuela[]={1,};
+  int hobby[]={2,};
+  int personalidad[]={2,};
+  int psexual[]={1,};
+  int genero[]={2,};
+  int pchilaquiles[]={2,};
+  int mascotas[]={1,};
+  int vida[]={2,};
 }
 
